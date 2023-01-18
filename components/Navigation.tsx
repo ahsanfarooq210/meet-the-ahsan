3ac64014ae8f3132f704b5f049b89fe93d21731f
RizwanAmjad <rizwanamjadms@gmail.com>
2023-01-18 21:27:32 +0500

    Created basic pages and run a Navigation component

---
+++ components/Navigation.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import styles from "@/styles/components/navigation.module.css"
+
+export default function Navigation() {
+  return (
+    <div>
+      <div>Navigation</div>
+      <nav className={styles.nav}>
+        <Link className={styles.navItem} href="/">
+          Home
+        </Link>
+        <Link className={styles.navItem} href="/certifications">
+          Certifications
+        </Link>
+        <Link className={styles.navItem} href="/projects">
+          Projects
+        </Link>
+      </nav>
+    </div>
+  )
+}
