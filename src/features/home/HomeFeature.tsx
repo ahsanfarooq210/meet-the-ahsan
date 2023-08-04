fef78baeaca1387e0fe0445091de1ce9e49806ba
ahsanfaroq210 <ahsanfarooq210@gmail.com>
2023-08-04 19:20:03 +0500

    home page in progress

---
+++ src/features/home/HomeFeature.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./HomeFeature.css";
+
+const HomeFeature = () => {
+  return (
+    <div className="home">
+      <div className="div1">
+        <img src="https://via.placeholder.com/500x1000" />
+      </div>
+
+      <div className="div2">
+        <h3>Hi There</h3>
+      </div>
+    </div>
+  );
+};
+
+export default HomeFeature;
